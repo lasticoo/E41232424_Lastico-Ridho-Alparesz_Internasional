e1f3a5d4fe3160543be2d66aa53cd1625b3da566
lasticoo <lastico.alparesz@gmail.com>
2024-02-11 12:27:21 +0700

    Create Lasticoo.java

---
+++ Lasticoo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lastico
+ */
+public class Lasticoo {
+    public static void  main (String [] Args) {
+        System.out.println("Lasticoo");
+        System.out.println("Lasticooo");
+        
+        System.out.print("Lasticoo");
+        System.out.print("Lasticooo");
+    }
+}
